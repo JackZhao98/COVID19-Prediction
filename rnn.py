16689bf384217c28fa1a8108ebcd69245cc4c7bd
Mingchao Lian <lianmccc@gmail.com>
2020-11-10 00:17:28 -0800

    implemented RNN LSTM Window method, py not finished

--- rnn.py
+++ rnn.py
@@ -8,39 +8,36 @@ from tensorflow.keras import layers
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, LSTM, Dropout
 
-# training_data set
+class RNN(object):
+    def __init__(self):
+        self = keras.Sequential()
+        self.X_train = []
+        self.y_train = []
+        self.X_test = []
+        self.y_test = []
+        self.window_size = 14
 
-X_train = []
-y_train = []
-X_test = []
-X_test = []
+    def train(self):
+        self.add(LSTM(units=60, activation='relu', return_sequences=True, input_shape=(self.X_train.shape[1], 7)))
+        self.add(Dropout(0.2))
 
-# window size 30 days
-for i in range(30, training_data.shape[0]):
-    X_train.append(training_data[i - 30:i])
-    y_train.append(training_data[i, 0])
+        self.add(LSTM(units=60, activation='relu', return_sequences=True))
+        self.add(Dropout(0.2))
 
-X_train, y_train = np.array(X_train), np.array(y_train)
+        self.add(LSTM(units=80, activation='relu', return_sequences=True))
+        self.add(Dropout(0.2))
 
-model = keras.Sequential()
+        self.add(LSTM(units=120, activation='relu'))
+        self.add(Dropout(0.2))
 
-model.add(LSTM(units = 60, activation = 'relu', return_sequences = True, input_shape = (X_train.shape[1], 10)))
-model.add(Dropout(0.2))
+        self.add(Dense(units=1))
 
-model.add(LSTM(units = 60, activation = 'relu', return_sequences = True))
-model.add(Dropout(0.2))
+    def fit(self):
+        self.compile(optimizer='adam', loss='mean_squared_error')
+        self.fit(self.X_train, self.y_train, epochs=50, batch_size=32)
 
-model.add(LSTM(units = 80, activation = 'relu', return_sequences = True))
-model.add(Dropout(0.2))
+    def pred(self):
+        y_pred = model.predict(X_test)
 
-model.add(LSTM(units = 120, activation = 'relu'))
-model.add(Dropout(0.2))
 
-model.add(Dense(units = 1))
 
-model.summary()
-
-model.compile(optimizer='adam', loss = 'mean_squared_error')
-model.fit(X_train, y_train, epochs=50, batch_size=32)
-
-y_pred = model.predict(X_test)
